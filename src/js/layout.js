4d1afa12cebae741b8e00d8214387a9f4e8d2cab
ManuelV85 <manuel.villater@gmail.com>
2023-02-18 14:07:07 +0000

    no muestra las cards

--- src/js/layout.js
+++ src/js/layout.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./views/home";
+import { Character } from "./views/Character";
+import { Planet } from "./views/Planets";
+import { Vehicle } from "./views/Vehicles";
 import { Demo } from "./views/demo";
 import { Single } from "./views/single";
 import injectContext from "./store/appContext";
@@ -10,28 +13,30 @@ import injectContext from "./store/appContext";
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 
-//create your first component
-const Layout = () => {
-	//the basename is used when your project is published in a subdirectory and not in the root of the domain
-	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-	const basename = process.env.BASENAME || "";
 
-	return (
-		<div>
-			<BrowserRouter basename={basename}>
-				<ScrollToTop>
-					<Navbar />
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/demo" element={<Demo />} />
-						<Route path="/single/:theid" element={<Single />} />
-						<Route path="*" element={<h1>Not found!</h1>} />
-					</Routes>
-					<Footer />
-				</ScrollToTop>
-			</BrowserRouter>
-		</div>
-	);
+const Layout = () => {
+    
+    const basename = process.env.BASENAME || "";
+
+    return (
+        <div>
+            <BrowserRouter basename={basename}>
+                <ScrollToTop>
+                    <Navbar />
+                    <Routes>
+                        <Route element={<Home />} path="/" />
+                        <Route element={<Demo />} path="/demo" />
+                        <Route element={<Character />} path="/characters/:uid" />
+                        <Route element={<Planet />} path="/planets/:uid" />
+                        <Route element={<Vehicle />} path="/vehicles/:uid" />
+                        <Route element={<Single />} path="/:type/:theid" />
+                        <Route element={<h1>Not found!</h1>} />
+                    </Routes>
+                    <Footer />
+                </ScrollToTop>
+            </BrowserRouter>
+        </div>
+    );
 };
 
 export default injectContext(Layout);
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,56 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useContext, useEffect } from "react";
+import { Context } from "../store/appContext";
 import "../../styles/home.css";
+import { ItemCard } from "../component/ItemCard";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+
+	const { store, actions } = useContext(Context);
+	// console.log(JSON.parse(localStorage.getItem("characters")));
+	return (
+		<React.Fragment>
+			<div className="container">
+				<h2 className="text-danger text-start my-4 display-4">Characters</h2>
+				<div className=" carousel mb-5">
+					<div className="itemcard gap-5">
+						{
+							store.characters.map((character, index) => {
+								return <ItemCard
+									item={character}
+									key={character._id}
+									endpoint="characters"
+									index={index} />
+							})
+						}
+					</div>
+				</div>
+				<h2 className="text-danger text-start my-4 display-4">Planets</h2>
+				<div className=" carousel mb-5">
+					<div className="itemcard gap-5">
+						{
+							store.planets.map((planet) => {
+								return <ItemCard
+									item={planet}
+									key={planet._id}
+									endpoint="planets" />
+							})
+						}
+					</div>
+				</div>
+				<h2 className="text-danger text-start my-4 display-4">Vehicles</h2>
+				<div className=" carousel mb-5">
+					<div className="itemcard gap-5">
+						{
+							store.vehicles.map((vehicle) => {
+								return <ItemCard
+									item={vehicle}
+									key={vehicle._id}
+									endpoint="vehicles" />
+							})
+						}
+					</div>
+				</div>
+			</div>
+		</React.Fragment>
+	);
+};
